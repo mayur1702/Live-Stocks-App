e98edfd89246dad70b2ceb897d96aa3aa37e323d
mayur1702 <m.mistry17@yahoo.com>
2019-03-16 16:43:12 +0530

    created view for the grid - dark theme

--- src/app/modules/stocks-grid/components/stock-grid-container/stock-grid-container.component.ts
+++ src/app/modules/stocks-grid/components/stock-grid-container/stock-grid-container.component.ts
@@ -10,6 +10,9 @@ export class StockGridContainerComponent implements OnInit {
 
   constructor(private stocksData:StockSocketService) { }
 
+  data={};
+  keys=[];
+
   ngOnInit() {
     this.stocksData.getStocksData().subscribe(
       (val) => { this.receivedData(val)}
@@ -19,8 +22,22 @@ export class StockGridContainerComponent implements OnInit {
   receivedData(dataValue){
     dataValue.map(x=>{
       console.log(`${x[0]}: ${x[1]}`)
+      let temp;
+      if(this.keys.indexOf(x[0])!=-1){
+        if(this.data[x[0]][0] < x[1]){
+          temp = true;
+        }
+        else{
+          temp = false;
+        }
+      }
+      else{
+        this.keys.push(x[0])
+      }
+      this.data[x[0]] = [Number((x[1]).toFixed(2)),temp,new Date()];
     });
   }
+
 
 
 }
